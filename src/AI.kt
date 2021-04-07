639e41e2606fc1ce8f1932f99cc75a4aff285408
Aya-AlNaqeep <ayaalnaqeep@gmail.com>
2021-04-07 15:05:44 +0300

    convert java code file to kotlin with no errors

---
+++ src/AI.kt
@@ -0,0 +1,6 @@
+class AI { //NOT YET USED
+    //easy - will make losing moves
+    //medium - will try to make a TicTacToe
+    //hard - will block other player's TicTacToe
+    //stupid - will make random moves (easiest to code)
+}
